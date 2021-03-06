cc1da9af263af3a271dcf7df61a883df5c8c8c29
Badruzzaman <badru.cse@gmail.com>
2021-03-06 20:45:47 +0600

    New one

--- array.js
+++ array.js
@@ -89,3 +89,5 @@ const fruits = ["apple", "banana", "cantaloupe", "blueberries", "grapefruit"];
 const index = fruits.findIndex(fruit => fruit === "blueberries");
 console.log(index); // 3
 console.log(fruits[index]); // blueberries
+
+console.log('new commit')
